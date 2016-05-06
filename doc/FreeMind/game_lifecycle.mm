7a25df589742ff4cfb35397fa5cf7f454ce12e16
kibertoad <iselwin@gmail.com>
2016-05-06 23:42:26 +0300

    Add lifecycle MindMap.

---
+++ doc/FreeMind/game_lifecycle.mm
@@ -0,0 +1,113 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1462565672457" ID="ID_988773056" MODIFIED="1462565689309" TEXT="SpringGameApplication">
+<node CREATED="1462565749994" ID="ID_1330966668" MODIFIED="1462565895396" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      create OpenGL context
+    </p>
+    <p>
+      (inside <b>Main</b>&#160;method)
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1462565980798" HGAP="28" ID="ID_1543490929" MODIFIED="1462566808011" VSHIFT="32">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Load game YML config
+    </p>
+    <p>
+      (inside <b>Create </b>method)
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1462566382735" ID="ID_1305177" MODIFIED="1462567068462">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Init Spring context
+    </p>
+    <p>
+      (inside <b>Create </b>method)
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1462566880524" HGAP="27" ID="ID_39304339" MODIFIED="1462567051110" VSHIFT="53">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Init Spring beans
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1462566887377" ID="ID_1807011429" MODIFIED="1462567063910">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Extract <b>GameState</b>&#160;beans
+    </p>
+    <p>
+      from Spring context
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1462566523058" HGAP="44" ID="ID_1931365219" MODIFIED="1462567019707" VSHIFT="52">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Load assets from&#160;<b>@LoadingState</b>,
+    </p>
+    <p>
+      using&#160;<b>@LoadOnStartup&#160;</b>annotated AssetLoaders
+    </p>
+    <p>
+      from Spring context
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1462566640365" HGAP="34" ID="ID_1038072605" MODIFIED="1462566818555" VSHIFT="59">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Switch to <b>@StartingState</b>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
