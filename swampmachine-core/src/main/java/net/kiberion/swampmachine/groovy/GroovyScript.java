fd24de1fc0bf7edb2037b54fa85de28108552e45
kibertoad <iselwin@gmail.com>
2016-05-27 00:57:38 +0300

    GroovyScript tweaked to actually be thread-safe (at a cost of increased memory usage, which still is negligible).

--- swampmachine-core/src/main/java/net/kiberion/swampmachine/groovy/GroovyScript.java
+++ swampmachine-core/src/main/java/net/kiberion/swampmachine/groovy/GroovyScript.java
@@ -1,21 +1,29 @@
 package net.kiberion.swampmachine.groovy;
 
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.stream.IntStream;
+
+import org.apache.commons.lang3.Validate;
+
 import groovy.lang.Binding;
 import groovy.lang.GroovyShell;
 import groovy.lang.Script;
-import net.kiberion.swampmachine.entities.common.api.Invokable;
+import net.kiberion.swampmachine.entities.common.api.ParametrizedInvokable;
 
 /**
  * @author kibertoad
  */
-public class GroovyScript implements Invokable {
+public class GroovyScript implements ParametrizedInvokable<Binding> {
+
+    private static final int POOL_SIZE = 5;
 
     private String scriptCode;
-    private Script compiledScript;
+    private BlockingDeque<Script> instancePool = new LinkedBlockingDeque<>(POOL_SIZE);
 
     public GroovyScript() {
     }
-    
+
     public GroovyScript(String scriptCode) {
         setScriptCode(scriptCode);
     }
@@ -23,20 +31,28 @@ public class GroovyScript implements Invokable {
     protected void setScriptCode(String toCode) {
         GroovyShell shell = new GroovyShell();
         scriptCode = toCode;
-        compiledScript = shell.parse(scriptCode);
-    }
 
-    /**
-     * Pass prepared binding context from processor to script
-     * @param binding
-     */
-    public void setBinding(Binding binding) {
-        compiledScript.setBinding(binding);
+        IntStream.rangeClosed(1, POOL_SIZE).forEach(counter -> {
+            instancePool.push(shell.parse(scriptCode));
+        });
+
     }
 
     @Override
-    public void invoke() {
-        compiledScript.run();
+    public void invoke(Binding binding) {
+        Validate.notNull(binding, "Binding cannot be null.");
+        Script script;
+        try {
+            script = instancePool.takeFirst();
+        } catch (InterruptedException e) {
+            throw new IllegalStateException(e);
+        }
+        try {
+            script.setBinding(binding);
+            script.run();
+        } finally {
+            instancePool.push(script);
+        }
     }
 
     @Override
@@ -44,7 +60,4 @@ public class GroovyScript implements Invokable {
         return scriptCode;
     }
 
-    public Script getCompiledScript() {
-        return compiledScript;
-    }
 }
