91337890b333a6065402f1d968dde36a5ebd5830
caryoscelus <caryoscelus@gmx.com>
2016-09-22 03:32:06 +0300

    fix compressor test portability
    - replace hardcoded file length value with dynamic check
    - do not check for exact compressed size as it may vary

--- swampmachine-utils/src/test/java/net/kiberion/swampmachine/utils/Bzip2CompressorTest.java
+++ swampmachine-utils/src/test/java/net/kiberion/swampmachine/utils/Bzip2CompressorTest.java
@@ -6,11 +6,6 @@ import net.kiberion.utils.compression.impl.BZip2Codec;
 public class Bzip2CompressorTest extends AbstractCompressorTest{
 
     @Override
-    protected int expectedCompressedSize() {
-        return 626;
-    }
-    
-    @Override
     protected Codec getCodec() {
         return new BZip2Codec();
     }
--- swampmachine-utils/src/test/java/net/kiberion/swampmachine/utils/SevenZCompressorTest.java
+++ swampmachine-utils/src/test/java/net/kiberion/swampmachine/utils/SevenZCompressorTest.java
@@ -6,11 +6,6 @@ import net.kiberion.utils.compression.impl.SevenZCodec;
 public class SevenZCompressorTest extends AbstractCompressorTest{
 
     @Override
-    protected int expectedCompressedSize() {
-        return 645;
-    }
-    
-    @Override
     protected Codec getCodec() {
         return new SevenZCodec();
     }
